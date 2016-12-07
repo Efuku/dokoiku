ae85fa750e1262d1383f5c02a6a29e4113a99f4b
Efuku <ef.eimu@gmail.com>
2016-12-07 22:18:44 +0900

    コメントを記述できるようにするためのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to:'top#index'
   # devise_for :users
 
-  resources :plans
+  resources :plans do
+    resources :comments, only: [:create]
+  end
   # get "/plans/index" => 'plans#index'
   # get "/plans/new" => 'plans#new'
   # post "/plans/new" => 'plans#create'
