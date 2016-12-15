01f011b72acacc02f9a9ddbd0c74afe617f5b9a3
Efuku <ef.eimu@gmail.com>
2016-12-15 22:11:58 +0900

    plansコントローラーにリダイレクトを設定

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,7 @@
 class PlansController < ApplicationController
+
+before_action :move_to_root, except: :index
+
   def index
    @plans = Plan.order('created_at DESC').page(params[:page]).per(5)
   end
@@ -22,5 +25,11 @@ class PlansController < ApplicationController
   def create_params
     params.require(:plan).permit(:destination, :season, :number_of_nights, :transportation, :budget, :remarks)
   end
+
+  def move_to_root
+  redirect_to "/" unless user_signed_in?
+  end
+
+
 
 end
