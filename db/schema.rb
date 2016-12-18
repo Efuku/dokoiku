f35368514c5a92be100c4e2ab3f65b696f727b18
Efuku <ef.eimu@gmail.com>
2016-12-18 18:23:10 +0900

    plansテーブルにmonthカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207135817) do
+ActiveRecord::Schema.define(version: 20161218091642) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",       limit: 4
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20161207135817) do
     t.integer  "user_id",          limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "month",            limit: 255
   end
 
   create_table "users", force: :cascade do |t|
