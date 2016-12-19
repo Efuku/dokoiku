391b58e7b760a36a12914b9e235488b588fc638d
Efuku <ef.eimu@gmail.com>
2016-12-19 22:36:05 +0900

    suggestionコントローラのcreateアクションを定義

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,2 +1,13 @@
 class SuggestionsController < ApplicationController
+
+  def create
+    @suggestion = Suggestion.create(create_params)
+  end
+
+  private
+  def create_params
+    params.require(:suggestion).permit(:text, :reference_url).merge(plan_id: params[:plan_id])
+    
+  end
+
 end
