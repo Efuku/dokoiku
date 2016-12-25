5e77c3a88d7143670f44b91c1951d171071430a7
Efuku <ef.eimu@gmail.com>
2016-12-26 00:55:41 +0900

    旅行記投稿の為のアクションを定義（一部） 投稿画面のビューを作成 マイページから旅行記投稿画面へのリンクを作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,4 +3,15 @@ class ReportsController < ApplicationController
   def index
     
   end
+
+  def new #投稿画面の作成
+    @report = Report.new
+  end
+
+  def create
+    #コメント投稿の処理を記載する
+  end
+
+
+
 end
