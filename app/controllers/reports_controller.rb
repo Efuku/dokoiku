574aedfba4dc298246393ed3dc843b0ea0796dd6
Efuku <ef.eimu@gmail.com>
2016-12-28 18:02:13 +0900

    旅行記投稿、旅行記一覧画面の表示、旅行記詳細画面の表示

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,7 @@
 class ReportsController < ApplicationController
 
   def index
-    
+    @reports = Report.order('created_at DESC').page(params[:page]).per(5)
   end
 
   def new #投稿画面の作成
@@ -9,9 +9,23 @@ class ReportsController < ApplicationController
   end
 
   def create
-    #コメント投稿の処理を記載する
+    Report.create(destination: create_params[:destination], month: create_params[:month], season: create_params[:season], number_of_nights: create_params[:number_of_nights], transportation: create_params[:transportation], budget: create_params[:budget], text: create_params[:text], user_id: current_user.id)
   end
 
+  def show
+    @report = Report.find(params[:id])
+    
+  end
+
+
+
+
+
+
+private
+  def create_params
+    params.require(:report).permit(:destination, :month, :season, :number_of_nights, :transportation, :budget, :text)
+  end
 
 
 end
