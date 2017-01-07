509944463bc721ba1b4b10b1b74aede61dbf7ca6
Efuku <ef.eimu@gmail.com>
2017-01-07 23:00:05 +0900

    paperclipを導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,19 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+        has_attached_file :avatar,
+                            styles: {medium: "100x100#", thumb: "100x100#"}
+        validates_attachment_content_type :avatar,
+                                          content_type: ["image/jpg", "image/png", "image/gif", "image/jpeg"]
+
+
          has_many :plans
          has_many :comments
          has_many :suggestions
          has_many :reports
+
+
+
+
+
 end
