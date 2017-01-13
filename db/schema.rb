713e05b1e0b4a7b105aa149047b7d75dae416d1d
Efuku <ef.eimu@gmail.com>
2017-01-13 22:06:50 +0900

    作成中のメモ的メッセージを削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161226144210) do
+ActiveRecord::Schema.define(version: 20170107123417) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",       limit: 4
@@ -75,6 +75,10 @@ ActiveRecord::Schema.define(version: 20161226144210) do
     t.string   "nickname",               limit: 255
     t.string   "residential_areas",      limit: 255
     t.text     "introduction",           limit: 65535
+    t.string   "avatar_file_name",       limit: 255
+    t.string   "avatar_content_type",    limit: 255
+    t.integer  "avatar_file_size",       limit: 4
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
